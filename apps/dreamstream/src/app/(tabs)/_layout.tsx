348c9878488ca505be40e0e20db46d7c9ac274e0
Act-Aks <akashsrivastava.git@gmail.com>
2025-08-24 15:41:06 +0530

    feat: 🎸 added tabs for the app

---
+++ apps/dreamstream/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabsLayout() {
+    return (
+        <Tabs screenOptions={{ headerShown: false }}>
+            <Tabs.Screen
+                name={'index'}
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color, size }) => <Ionicons color={color} name="home" size={size} />,
+                }}
+            />
+            <Tabs.Screen
+                name={'search'}
+                options={{
+                    title: 'Search',
+                    tabBarIcon: ({ color, size }) => <Ionicons color={color} name="search" size={size} />,
+                }}
+            />
+            <Tabs.Screen
+                name={'downloads'}
+                options={{
+                    title: 'Downloads',
+                    tabBarIcon: ({ color, size }) => <Ionicons color={color} name="download" size={size} />,
+                }}
+            />
+            <Tabs.Screen
+                name={'settings'}
+                options={{
+                    title: 'Settings',
+                    tabBarIcon: ({ color, size }) => <Ionicons color={color} name="settings" size={size} />,
+                }}
+            />
+        </Tabs>
+    );
+}
