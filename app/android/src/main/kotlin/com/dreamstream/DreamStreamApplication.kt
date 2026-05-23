4d6afe6fc9e5f8b4013283e7fce4625d9e62b051
Act-Aks <akashsrivastava.git@gmail.com>
2026-05-23 20:31:16 +0530

    style(app): sort imports in DreamStreamApplication lexicographically

--- app/android/src/main/kotlin/com/dreamstream/DreamStreamApplication.kt
+++ app/android/src/main/kotlin/com/dreamstream/DreamStreamApplication.kt
@@ -1,10 +1,10 @@
 package com.dreamstream
 
 import android.app.Application
-import com.dreamstream.feature.home.data.di.homeDataModule
-import com.dreamstream.feature.home.presentation.di.homePresentationModule
 import com.dreamstream.feature.details.data.di.detailsDataModule
 import com.dreamstream.feature.details.presentation.di.detailsPresentationModule
+import com.dreamstream.feature.home.data.di.homeDataModule
+import com.dreamstream.feature.home.presentation.di.homePresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
